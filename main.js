7fb2f805168f7bbb32db491d2b5d041d0c8b42d9
Will <vclwei@qq.com>
2025-01-14 19:13:11 +0800

    fix: Add the Origin field to the http headers

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ const headers = {
     "Sec-Ch-Ua-Mobile": "?0",
     "Sec-Ch-Ua-Platform": '"Windows"',
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Origin": "chrome-extension://ekbbplmjjgoobhdlffmgeokalelnmjjc"
 }
 
 function readFile(pathFile) {
